ee085f11ad2818fbf8480dffd97358e5cdad0e70
Stephane Torres <stephane.torres@madetech.com>
2024-03-21 18:03:48 +0000

    tests made for BMI problem, intial commit

---
+++ test/bmi_tests.py
@@ -0,0 +1,7 @@
+def test_bmi_returns_correct_string():
+    assert(bmi(50, 1.8)) == "Underweight"
+    assert(bmi(80, 1.8)) == "Normal"
+    assert(bmi(90, 1.8)) == "Oveerweight"
+    assert(bmi(110, 1.8)) == "Obese"
+    assert(bmi(50, 1.5)) == "Normal"
+    
