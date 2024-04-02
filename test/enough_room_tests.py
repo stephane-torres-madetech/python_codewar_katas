0123649acfaf6edfa31322f9e07c3a1c6415bb52
Stephane Torres <stephane.torres@madetech.com>
2024-04-02 12:01:45 +0100

    test:RED enough_room(100, 60, 50) => 10 bus does not have enough room, should return how many ppl left

--- test/enough_room_tests.py
+++ test/enough_room_tests.py
@@ -1,5 +1,9 @@
 from ..codewar_problems.will_there_be_enough_space_on_the_bus import enough_room
 
-def test_enough_room_on_the_bus():
+def test_bus_has_enough_room():
     assert enough_room(10, 5, 5) == 0
+    assert enough_room(20, 15, 5) == 0
+
+def test_bus_does_not_have_enough_room():
+    assert enough_room(100, 60, 50) == 10 
     
