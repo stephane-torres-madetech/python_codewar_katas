485f59efe41cb66b5ac9b45ed979bd869ba7b586
Stephane Torres <stephane.torres@madetech.com>
2024-04-02 09:50:20 +0100

    test:RED next simplest case, [1, 2, 3] => [1, 3]

--- test/remove_every_other_from_list_tests.py
+++ test/remove_every_other_from_list_tests.py
@@ -2,4 +2,5 @@ from ..codewar_problems.remove_every_other_from_list import remove_every_other
 
 def test_removes_every_other_from_list():
     assert remove_every_other([1, 2]) == [1]
+    assert remove_every_other([1, 2, 3]) == [1, 3]
     # assert remove_every_other([2, 3, 4, 5, 6, 7, 8]) == [2, 4, 6, 8]
