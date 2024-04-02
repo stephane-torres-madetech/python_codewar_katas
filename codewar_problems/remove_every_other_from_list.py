b19dcdcaa98dce57a362d289602be204c4e6d306
Stephane Torres <stephane.torres@madetech.com>
2024-04-02 10:30:56 +0100

    other solution, the one i was trying to explore, needed a new list to hold values and not mutate/pop from the initial list

--- codewar_problems/remove_every_other_from_list.py
+++ codewar_problems/remove_every_other_from_list.py
@@ -11,3 +11,14 @@ def remove_every_other(array: list):
    """ list[start point for slice : end point(not inclusive for slice : step/increment)] 
        if start and end are left blank e.g array[::2] start is assumed at 0 and end is assumed length of list"""
    return array[::2] 
+
+
+""" def remove_every_other(my_list):
+    r = []
+    for i in range(len(my_list)):
+        if i % 2 == 0:
+            r.append(my_list[i])
+    return r 
+    
+    ==> this is what i was trying to do earlier for my for loop
+    """
