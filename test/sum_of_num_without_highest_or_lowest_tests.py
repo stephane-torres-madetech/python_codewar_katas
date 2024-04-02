ac6e3e3a5c30a29dac320e57df489e78bf45e6c4
Stephane Torres <stephane.torres@madetech.com>
2024-04-02 11:04:53 +0100

    test:GREEN lists with one or two or no elements return 0

--- test/sum_of_num_without_highest_or_lowest_tests.py
+++ test/sum_of_num_without_highest_or_lowest_tests.py
@@ -2,3 +2,11 @@ from ..codewar_problems.sum_of_nums_without_highest_or_lowest import get_sum_fro
 
 def test_empty_list():
     assert get_sum_from_list([]) == 0
+    assert get_sum_from_list(None) == 0
+
+def test_lists_with_only_one_element():
+    assert get_sum_from_list([1]) == 0
+
+def test_lists_with_only_two_elements():
+    assert get_sum_from_list([3, 5]) == 0
+    assert get_sum_from_list([-3, -5]) == 0
