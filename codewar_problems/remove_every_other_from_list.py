73f851540b87f89d04aad8a514865e8c2621e161
Stephane Torres <stephane.torres@madetech.com>
2024-04-02 09:54:15 +0100

    test:GREEN [1, 2, 3] => [1, 3]

---
+++ codewar_problems/remove_every_other_from_list.py
@@ -0,0 +1,10 @@
+""" Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+Example:
+["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+None of the arrays will be empty, so you don't have to worry about that! """
+
+def remove_every_other(array: list):
+    array.pop(1)
+    return array
