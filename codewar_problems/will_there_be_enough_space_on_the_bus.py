0d21029eaef0f8936bfab61b6e23014460e63d8a
Stephane Torres <stephane.torres@madetech.com>
2024-04-02 11:59:23 +0100

    test:GREEN enough_room(10, 5, 5) => 0)

--- codewar_problems/will_there_be_enough_space_on_the_bus.py
+++ codewar_problems/will_there_be_enough_space_on_the_bus.py
@@ -16,3 +16,6 @@ Usage Examples:
 cap = 10, on = 5, wait = 5 --> 0 # He can fit all 5 passengers
 cap = 100, on = 60, wait = 50 --> 10 # He can't fit 10 of the 50 waiting
  """
+
+def enough_room(cap, on, wait):
+    return 0
--- test/enough_room_tests.py
+++ test/enough_room_tests.py
@@ -1,3 +1,5 @@
+from ..codewar_problems.will_there_be_enough_space_on_the_bus import enough_room
+
 def test_enough_room_on_the_bus():
     assert enough_room(10, 5, 5) == 0
     
