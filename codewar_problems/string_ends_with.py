7fbfda62e042aabeb4a4422ea808e9fb0719eeee
Stephane Torres <stephane.torres@madetech.com>
2024-05-29 11:55:57 +0100

    REFACTOR, remove extra variable

--- codewar_problems/string_ends_with.py
+++ codewar_problems/string_ends_with.py
@@ -3,7 +3,6 @@ def string_ends_with(text, ending):
     # get length of ending
     # take last letters of same length and compare to ending, return same 
 
-    ending_length = len(ending)
-    text_end = text[-ending_length:]
+    text_end = text[-len(ending):]
     return text_end == ending
     return True
