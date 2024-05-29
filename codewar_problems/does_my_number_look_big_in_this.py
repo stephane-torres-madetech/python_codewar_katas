4b17d0b04c29a13e673de2ede68b81fbe83daeda
Stephane Torres <stephane.torres@madetech.com>
2024-05-29 13:56:56 +0100

    test:GREEN helper functions defined, return true or false based on if number is an armstrong number with length as power

--- codewar_problems/does_my_number_look_big_in_this.py
+++ codewar_problems/does_my_number_look_big_in_this.py
@@ -1,2 +1,51 @@
-def narcissistic(num):
-    return True
+"""
+A Narcissistic Number (or Armstrong Number) is a positive number which is the sum of its own digits,
+each raised to the power of the number of digits in a given base. In this Kata, we will restrict ourselves to decimal (base 10).
+
+For example, take 153 (3 digits), which is narcissistic:
+    1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+
+and 1652 (4 digits), which isn't:
+    1^4 + 6^4 + 5^4 + 2^4 = 1 + 1296 + 625 + 16 = 1938
+The Challenge:
+
+Your code must return true or false (not 'true' and 'false') depending upon whether the given number is a Narcissistic number in base 10.
+
+This may be True and False in your language, e.g. PHP.
+
+Error checking for text strings or other invalid inputs is not required, only valid positive non-zero integers will be passed into the function.
+
+
+so want to take a number, split into individual digits, raise each of these to the power of the original numbers length? 
+helper function to split number and return an array """
+
+def narcissistic(number):
+
+    length = len(str(number))
+
+    number_list = split(number)
+    modified_number_list = []
+
+    # remember naming classes in for loops
+    for digit in number_list:
+        modified_number_list.append(digit ** length)
+
+    return sum_array(modified_number_list) == number
+
+def split(number):
+    
+    number_string = str(number)
+    number_list = []
+
+    for i in range(len(number_string)):
+        number_list.append(int(number_string[i]))
+
+    return number_list
+
+def sum_array(list):
+    result = 0
+
+    for number in list:
+        result += number
+
+    return result
--- test/test_does_my_number_look_big_in_this.py
+++ test/test_does_my_number_look_big_in_this.py
@@ -1,4 +1,14 @@
-from ..codewar_problems.does_my_number_look_big_in_this import narcissitic
+from ..codewar_problems.does_my_number_look_big_in_this import narcissistic, split, sum_array
 
-def test_narcissistic_153():
-    assert narcissitic(153) == True
+def test_153_is_narcissisic():
+    assert narcissistic(153) == True
+
+def test_122_is_not_narcissistic():
+    assert narcissistic(122) == False
+
+def test_number_splitter():
+    assert split(153) == [1, 5, 3]
+
+def test_sum_array():
+    assert sum_array([1, 2, 3]) == 6
+    assert sum_array([1, 2, 3, 4, 5]) == 15
